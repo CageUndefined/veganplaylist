4cf2aeb8c0f8cce4e4474a8a409e74045cdb9aa0
Tristan <tristanmkernan@gmail.com>
2019-03-15 09:33:10 -0400

    feat: add playlist model, migrations

---
+++ database/migrations/2019_03_15_130038_create_playlist_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlaylistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('playlists', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+
+            $table->unsignedBigInteger('creator_id');
+            $table->foreign('creator_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('playlists');
+    }
+}
