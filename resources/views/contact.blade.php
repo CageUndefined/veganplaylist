cdb4b0168ac8126775da27946fe4d7cfbfdb457d
David Van Beveren <david.vanbeveren@gmail.com>
2019-03-26 11:21:03 +0100

    Contact page changes

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.page')
+
+@section('page_content')
+    <div class="container">
+<iframe src="https://youaretheirvoice.com/contact?veganplaylist" style="position:fixed; left:0; bottom:-55px; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+    Your browser doesn't support iframes
+</iframe>
+
+    </div>
+@endsection
