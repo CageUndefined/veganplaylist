5c534ced1f764a51a3bc8e9863a601905462beeb
David Van Beveren <david.vanbeveren@gmail.com>
2019-03-26 10:54:44 +0100

    Homepage layout by Charles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,18 @@
 @extends('layouts.page')
 
+@php for( $i = 0; $i < 10; $i++ ) $playlists[] = $playlists[ 0 ]; @endphp
+
 @section('page_content')
     <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <div class="I-dont-know-what-im-doing-frontend-is-scary" style="float: right;">
-                    <form action="{{ route( 'playlist.create' ) }}" method="get">
-                        <input name="name" placeholder="Playlist name (optional)" />
-                        <button class="btn btn-primary">Create Playlist</button>
+		<center>
+        <b style="    font-size: 30px;">Find and share inspirational vegan videos</b><br>
+		<b style="    font-size: 18px;">Spread the compassionate vegan message with our curated playlists, or create your own from our huge video library.</b><br><br>
+			 <form action="{{ route( 'playlist.create' ) }}" method="get">
+                        <input style="    width: 273px;
+    padding: 6px;"name="name" placeholder="Enter a playlist name to start" />
+                        <button class="btn btn-primary">Create</button>
                     </form>
-                </div>
-                @guest
-                    Welcome, Guest! Please convert to veganism.<br/>
-                @else
-                    @if(session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    Welcome! You are logged in!<br/>
-                @endguest
-            </div>
-        </div>
+       </center>
         <br><br>
         <div class="card-columns">
            @foreach( $playlists as $playlist )
