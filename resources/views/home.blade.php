e4b8129453157624a0214cfc6947b67b1994fb0a
David Van Beveren <david.vanbeveren@gmail.com>
2019-03-26 13:57:12 +0100

    Better Create Playlist Form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.page')
 
-@php for( $i = 0; $i < 10; $i++ ) $playlists[] = $playlists[ 0 ]; @endphp
-
 @section('page_content')
     <div class="container">
 		<center>
         <b style="    font-size: 30px;">Find and share inspirational vegan videos</b><br>
 		<b style="    font-size: 18px;">Spread the compassionate vegan message with our curated playlists, or create your own from our huge video library.</b><br><br>
-			 <form action="{{ route( 'playlist.create' ) }}" method="get">
-                        <input style="    width: 273px;
-    padding: 6px;"name="name" placeholder="Enter a playlist name to start" />
-                        <button class="btn btn-primary">Create</button>
-                    </form>
+	
+	
+	<form action="{{ route( 'playlist.create' ) }}" method="get">
+				
+	<div class="input-group mb-3">
+	<input style="width: 273px;padding: 6px;" name="name" placeholder="Enter a playlist name to start" />
+	<div class="input-group-append">
+	<button class="btn btn-outline-secondary" style="background-color: #349de4;color: #fff;"type="button" id="button-addon2">Create</button>
+	</div>
+	</div>
+
+	</form>
+	
        </center>
         <br><br>
         <div class="card-columns">
