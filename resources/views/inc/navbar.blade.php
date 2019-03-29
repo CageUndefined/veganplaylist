ba0e9830222985f3c0013b4d6c862cd92a44b369
David Van Beveren <david.vanbeveren@gmail.com>
2019-03-29 20:43:13 +0100

    Logo updated

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,7 +1,7 @@
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark navbar-laravel">
     <div class="container-fluid">
-        <a href="https://veganplaylist.org/"><img src="https://i.imgur.com/guJfRhx.png" style="width: 300px;"></a>
+        <a href="https://veganplaylist.org/"><img src="https://i.imgur.com/Hfkiumn.png" style="width: 300px;"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
