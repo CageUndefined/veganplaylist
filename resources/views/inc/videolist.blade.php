9599553809ce4fe1e864b7d808be2398cfb2e093
Gerard O'Neill <grardb@gmail.com>
2019-03-31 21:53:47 -0700

    Add playlist preview indicator + made modal bigger

--- resources/views/inc/videolist.blade.php
+++ resources/views/inc/videolist.blade.php
@@ -1,8 +1,9 @@
 
 @foreach ( $videos as $video )
     <div class="card video-card" id="card_{{ $video->id }}">
-        <a href="{{$video->getEmbedSrcAttribute()}}}" class="playlist-editor-thumbnail" data-title="{{$video->title}}">
+        <a href="{{$video->getEmbedSrcAttribute()}}}" class="playlist-editor-thumbnail position-relative d-inline-block" data-title="{{$video->title}}">
             <img class="card-img-top" src="{{ $video->getThumbnailSrcAttribute() }}" />
+            <div class="play-button"></div>
         </a>
         <div class="card-body text-center">
             <p class="card-text">{{ $video->title }} - <span class="text-secondary">{{ $video->getTime() }}</span></p>
