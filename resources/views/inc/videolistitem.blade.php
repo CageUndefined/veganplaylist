d267200038cebcb5b9bb204d523b3a583982e849
Gerard O'Neill <grardb@gmail.com>
2019-03-31 23:07:27 -0700

    Add visual indicators for re-ordering playlist videos

--- resources/views/inc/videolistitem.blade.php
+++ resources/views/inc/videolistitem.blade.php
@@ -1,8 +1,11 @@
-<li class="list-group-item" id="list_item_{{ $video->id }}" data-id="{{ $video->id }}">
+<li class="playlist-list-group-item list-group-item" id="list_item_{{ $video->id }}" data-id="{{ $video->id }}">
     <div class="d-flex align-items-center flex-row">
+        <div class="mr-3">
+            <i class="fas fa-bars"></i>
+        </div>
         <div class="mr-2">@include('inc.videothumb')</div>
         <div class="font-weight-bold text-wrap title">{{ $video->title }}</div>
-        <button data-id="{{ $video->id }}" class="remove text-danger ml-2 border-0 bg-transparent">
+        <button data-id="{{ $video->id }}" class="remove text-danger ml-2 border-0 bg-transparent flex-fill text-right">
             <i class="fas fa-trash-alt"></i>
         </button>
     </div>
